8e552d009adcdc27db1f7aca8672b470552c0931
analogy47 <actweed47@gmail.com>
2018-10-27 12:44:48 -0400

    added axios and GET request to render smurfs

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -6,28 +7,35 @@ class SmurfForm extends Component {
     this.state = {
       name: '',
       age: '',
-      height: ''
+      height: '',
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    };
     this.setState({
       name: '',
       age: '',
-      height: ''
+      height: '',
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  // create smurf object to post
+
   render() {
     return (
       <div className="SmurfForm">
+        {/* form to add a smurf */}
         <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
